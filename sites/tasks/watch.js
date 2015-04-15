0d9f93d3a1ca80da0718f99b1d284aaf36330e41
Masaya Yamauchi <masaya.yamauchi@gmail.com>
2015-04-15 10:44:47 +0900

    specファイルに変更があった場合、テストも実行するようにした。

--- sites/tasks/watch.js
+++ sites/tasks/watch.js
@@ -2,8 +2,8 @@
 
 var gulp    = require('gulp');
 
-gulp.task('watch', ['lint', 'build'], function(){
+gulp.task('watch', ['lint', 'build', 'spec'], function(){
   gulp.watch('./src/less/**/*.less', ['build-less']);
   gulp.watch('./src/js/**/*.js',     ['lint', 'build-js']);
-  gulp.watch('./spec/**/*.js',       ['lint', 'build-spec']);
+  gulp.watch('./spec/**/*.js',       ['lint', 'spec']);
 });

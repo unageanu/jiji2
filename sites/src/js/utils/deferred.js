072f07a118e18e6cf1c3e53331a87a346f091280
Masaya Yamauchi <masaya.yamauchi@gmail.com>
2015-04-18 10:11:21 +0900

    Promiseを使うDeferredの実装もコメントに残しておく。
    * babel/polyfillの実装だとcallbackがsetTimeoutで遅延実行され、期待通り動作しない箇所があるため、利用していない。

--- sites/src/js/utils/deferred.js
+++ sites/src/js/utils/deferred.js
@@ -1,3 +1,19 @@
 import ContainerJS from "container-js"
 
 export default ContainerJS.utils.Deferred;
+
+// export default class Deferred {
+//   constructor() {
+//     this.promise = new Promise(this.callback.bind(this));
+//   }
+//   callback(resolve, reject) {
+//     this.resolve = resolve;
+//     this.reject = reject;
+//   }
+//   promise() {
+//     return this.promise;
+//   }
+//   then( success, fail ) {
+//     return this.promise.then(success, fail);
+//   }
+// }

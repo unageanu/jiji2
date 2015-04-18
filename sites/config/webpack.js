309a898b00031e94d63636eae0e910ff6a429b99
Masaya Yamauchi <masaya.yamauchi@gmail.com>
2015-04-18 12:55:52 +0900

    webpackの設定ファイルをリファクタリング。
    * 重複を排除した。

--- sites/config/webpack.js
+++ sites/config/webpack.js
@@ -1,57 +1,53 @@
 "use strict";
 
 var webpack = require("webpack");
+var merge   = require('deepmerge')
 
 var base = {
-    module: {
-        loaders: [{
-            test:     /\.js$/,
-            exclude:  /(node_modules|lib)/,
-            loader:  'babel-loader'
-        }],
-
-        exprContextRecursive : true,
-        exprContextRegExp: /^\.\/(?!main)([a-zA-Z0-9\-\_\/]*)$/,
-        exprContextCritical: false,
-        exprContextRequest: '../../../src/js',
-
-        unknownContextRegExp: /$^/,
-        unknownContextCritical: false
+  resolve: {
+    alias: {
+      msgpack: __dirname + '/../lib/msgpack.codec.js'
     }
+  },
+  plugins:  [
+    new webpack.IgnorePlugin(/vertx/)
+  ],
+  module: {
+    loaders: [{
+      test:     /\.js$/,
+      exclude:  /(node_modules|lib)/,
+      loader:  'babel-loader'
+    }],
+
+    exprContextRecursive : true,
+    exprContextRegExp: /^\.\/(?!main)([a-zA-Z0-9\-\_\/]*)$/,
+    exprContextCritical: false,
+    exprContextRequest: '../../../src/js',
+
+    unknownContextRegExp: /$^/,
+    unknownContextCritical: false
+  }
 };
 
-var alias = {
-  src:     __dirname + '/../src/js',
-  msgpack: __dirname + '/../lib/msgpack.codec.js'
-};
-var plugins =  [
-    new webpack.IgnorePlugin(/vertx/)
-];
+function createConfig( root, mainFile, options) {
+  return merge( merge( base, {
+    entry: '.' + root + '/' + mainFile,
+    output: {
+      filename: mainFile
+    },
+    resolve: {
+      root: __dirname + root
+    }
+  }), options || {});
+}
 
 module.exports = {
-    src : {
-        entry: './src/js/main.js',
-        output: {
-            filename: 'main.js'
-        },
-        resolve: {
-            root: __dirname + '/src/js',
-            alias: alias
-        },
-        module: base.module,
-        plugins: plugins
-    },
-
-    spec: {
-        entry: './spec/all-specs.js',
-        output: {
-            filename: 'all-specs.js'
-        },
-        resolve: {
-            root: __dirname + '/spec',
-            alias: alias
-        },
-        module: base.module,
-        plugins: plugins
+  src : createConfig( '/src/js', 'main.js'),
+  spec: createConfig( '/spec',   'all-specs.js', {
+    resolve: {
+      alias: {
+        src:     __dirname + '/../src/js'
+      }
     }
+  })
 }

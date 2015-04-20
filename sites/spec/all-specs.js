cb1845fa0e9dc05880b4800271c18033bb74718d
Masaya Yamauchi <masaya.yamauchi@gmail.com>
2015-04-20 15:39:00 +0900

    Objectのプロパティをスキャンするユーティリティを作成。
    * 内部で利用するため、underscore.jsも依存モジュールに追加した。

--- sites/spec/all-specs.js
+++ sites/spec/all-specs.js
@@ -2,3 +2,4 @@ import "./specs/model/preferences-spec";
 import "./specs/remoting/url-resolver-spec";
 import "./specs/remoting/xhr-manager-spec";
 import "./specs/utils/observable-spec";
+import "./specs/utils/objects-spec";
---
+++ sites/src/js/utils/objects.js
@@ -0,0 +1,26 @@
+import _ from "underscore";
+
+export default class Objects {
+
+  static traverseValues(object, fn) {
+    Objects.convert(object, fn, null);
+  }
+
+  static convert(object, converter, key=null) {
+    if ( _.isArray(object) ) {
+      return object.map(
+          (item) => this.convert(item, converter, key) );
+    } else if (_.isFunction(object)
+            || _.isDate(object)
+            || _.isRegExp(object)
+            || !_.isObject(object) ) {
+      return converter(object, key);
+    } else {
+      return _.keys(object).reduce( (r, k) => {
+        r[k] = this.convert(object[k], converter, k);
+        return r;
+      }, {});
+    }
+  }
+
+}

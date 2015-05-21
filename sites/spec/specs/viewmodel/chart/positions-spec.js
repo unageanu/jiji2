6a60c0e1ced661b98b1c4ba0fa3e544b4517da54
Masaya Yamauchi <masaya.yamauchi@gmail.com>
2015-05-21 16:06:38 +0900

    indentを調整。

--- sites/spec/specs/viewmodel/chart/positions-spec.js
+++ sites/spec/specs/viewmodel/chart/positions-spec.js
@@ -69,58 +69,58 @@ describe("Positions", () => {
   });
 
 
-    it("endがnullの場合、未決済と判定される。", () => {
-      operator.initialize(1000, 300, "fifteen_minutes");
-      chart.slider.preferences.chartInterval = "one_hour";
+  it("endがnullの場合、未決済と判定される。", () => {
+    operator.initialize(1000, 300, "fifteen_minutes");
+    chart.slider.preferences.chartInterval = "one_hour";
 
-      const requests = chart.slider.rates.rateService.xhrManager.requests;
-      requests[0].resolve(operator.createRates([
-        {high:179.0, low:178.0, open:178.2, close:178.5, timestamp:Dates.date("2015-05-08T10:00:00Z")}
-      ]));
-      requests[1].resolve([
-        {enteredAt:Dates.date("2015-05-08T09:00:10Z"), exitedAt:Dates.date("2015-05-08T15:30:00Z")},
-        {enteredAt:Dates.date("2015-05-08T01:30:10Z")},
-        {enteredAt:Dates.date("2015-05-08T16:10:10Z")},
-        {enteredAt:Dates.date("2015-05-08T19:30:10Z")},
-        {enteredAt:Dates.date("2015-05-08T16:30:10Z"), exitedAt:Dates.date("2015-05-08T18:26:01Z")}
-      ]);
+    const requests = chart.slider.rates.rateService.xhrManager.requests;
+    requests[0].resolve(operator.createRates([
+      {high:179.0, low:178.0, open:178.2, close:178.5, timestamp:Dates.date("2015-05-08T10:00:00Z")}
+    ]));
+    requests[1].resolve([
+      {enteredAt:Dates.date("2015-05-08T09:00:10Z"), exitedAt:Dates.date("2015-05-08T15:30:00Z")},
+      {enteredAt:Dates.date("2015-05-08T01:30:10Z")},
+      {enteredAt:Dates.date("2015-05-08T16:10:10Z")},
+      {enteredAt:Dates.date("2015-05-08T19:30:10Z")},
+      {enteredAt:Dates.date("2015-05-08T16:30:10Z"), exitedAt:Dates.date("2015-05-08T18:26:01Z")}
+    ]);
 
-      expect(extract(positions.positionsForDisplay)).toEq([
-        [{
-          normalizedStart: Dates.date("2015-05-08T09:00:00Z"),
-          normalizedEnd: Dates.date("2015-05-08T15:00:00Z"),
-          startX: 5,
-          endX: 41
-        }, {
-          normalizedStart: Dates.date("2015-05-08T16:00:00Z"),
-          normalizedEnd: null,
-          startX: 47,
-          endX: undefined
-        }],
-        [{
-          normalizedStart: Dates.date("2015-05-08T01:00:00Z"),
-          normalizedEnd: null,
-          startX: -43,
-          endX: undefined
-        }],
-        [{
-          normalizedStart: Dates.date("2015-05-08T19:00:00Z"),
-          normalizedEnd: null,
-          startX: 65,
-          endX: undefined
-        }, {
-          normalizedStart: Dates.date("2015-05-08T16:00:00Z"),
-          normalizedEnd: Dates.date("2015-05-08T18:00:00Z"),
-          startX: 47,
-          endX: 59
-        }],
-        [],
-        [],
-        [],
-        [],
-        []
-      ]);
-    });
+    expect(extract(positions.positionsForDisplay)).toEq([
+      [{
+        normalizedStart: Dates.date("2015-05-08T09:00:00Z"),
+        normalizedEnd: Dates.date("2015-05-08T15:00:00Z"),
+        startX: 5,
+        endX: 41
+      }, {
+        normalizedStart: Dates.date("2015-05-08T16:00:00Z"),
+        normalizedEnd: null,
+        startX: 47,
+        endX: undefined
+      }],
+      [{
+        normalizedStart: Dates.date("2015-05-08T01:00:00Z"),
+        normalizedEnd: null,
+        startX: -43,
+        endX: undefined
+      }],
+      [{
+        normalizedStart: Dates.date("2015-05-08T19:00:00Z"),
+        normalizedEnd: null,
+        startX: 65,
+        endX: undefined
+      }, {
+        normalizedStart: Dates.date("2015-05-08T16:00:00Z"),
+        normalizedEnd: Dates.date("2015-05-08T18:00:00Z"),
+        startX: 47,
+        endX: 59
+      }],
+      [],
+      [],
+      [],
+      [],
+      []
+    ]);
+  });
 
 
   it("同時刻に8以上ポジションがある場合、表示されない。", () => {

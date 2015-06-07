cf30eda259a9972ed4e27ba1cd622d10453f3533
Masaya Yamauchi <masaya.yamauchi@gmail.com>
2015-06-07 20:40:52 +0900

    should_close?, update_priceを実装。

---
+++ spec/jiji/model/trading/closing_policy_spec.rb
@@ -0,0 +1,141 @@
+# coding: utf-8
+
+require 'jiji/test/test_configuration'
+require 'jiji/test/data_builder'
+
+describe Jiji::Model::Trading::ClosingPolicy do
+  describe '#should_close?' do
+    it 'take_profitが設定されている場合、利益が設定値を上回った時点で決済される' do
+      policy = Jiji::Model::Trading::ClosingPolicy.create({
+        take_profit: 130
+      })
+      position = Jiji::Model::Trading::Position.new do |p|
+        p.entry_price = 120
+        p.current_price = 125
+        p.sell_or_buy = :buy
+      end
+
+      expect(policy.should_close?(position)).to be false
+      position.current_price = 130
+      expect(policy.should_close?(position)).to be true
+      position.current_price = 131
+      expect(policy.should_close?(position)).to be true
+
+      position = Jiji::Model::Trading::Position.new do |p|
+        p.entry_price = 140
+        p.current_price = 135
+        p.sell_or_buy = :sell
+      end
+
+      expect(policy.should_close?(position)).to be false
+      position.current_price = 130
+      expect(policy.should_close?(position)).to be true
+      position.current_price = 129
+      expect(policy.should_close?(position)).to be true
+    end
+
+    it 'stop_lossが設定されている場合、損失が設定値を超えた時点で決済される' do
+      policy = Jiji::Model::Trading::ClosingPolicy.create({
+        stop_loss: 110
+      })
+      position = Jiji::Model::Trading::Position.new do |p|
+        p.entry_price = 120
+        p.current_price = 125
+        p.sell_or_buy = :buy
+      end
+
+      expect(policy.should_close?(position)).to be false
+      position.current_price = 110
+      expect(policy.should_close?(position)).to be true
+      position.current_price = 109
+      expect(policy.should_close?(position)).to be true
+
+      position = Jiji::Model::Trading::Position.new do |p|
+        p.entry_price = 100
+        p.current_price = 95
+        p.sell_or_buy = :sell
+      end
+
+      expect(policy.should_close?(position)).to be false
+      position.current_price = 110
+      expect(policy.should_close?(position)).to be true
+      position.current_price = 111
+      expect(policy.should_close?(position)).to be true
+    end
+
+    it 'trailing_stopが設定されている場合、損失が値を超えた時点で決済される' do
+      policy = Jiji::Model::Trading::ClosingPolicy.create({
+        trailing_stop:   50,
+        trailing_amount: 110
+      })
+      position = Jiji::Model::Trading::Position.new do |p|
+        p.entry_price = 120
+        p.current_price = 125
+        p.sell_or_buy = :buy
+      end
+
+      expect(policy.should_close?(position)).to be false
+      position.current_price = 110
+      expect(policy.should_close?(position)).to be true
+      position.current_price = 109
+      expect(policy.should_close?(position)).to be true
+
+      position = Jiji::Model::Trading::Position.new do |p|
+        p.entry_price = 100
+        p.current_price = 95
+        p.sell_or_buy = :sell
+      end
+
+      expect(policy.should_close?(position)).to be false
+      position.current_price = 110
+      expect(policy.should_close?(position)).to be true
+      position.current_price = 111
+      expect(policy.should_close?(position)).to be true
+    end
+  end
+
+  describe '#update_price' do
+    it 'trailing_stopが設定されていれば、trailing_amountが更新される' do
+      pair = Jiji::Model::Trading::Pair.new(
+        :EURJPY, 'EUR_JPY', 0.01,   10_000_000, 0.001,   0.04)
+
+      policy = Jiji::Model::Trading::ClosingPolicy.create({
+        trailing_stop:   50,
+        trailing_amount: 0
+      })
+      position = Jiji::Model::Trading::Position.new do |p|
+        p.entry_price = 120
+        p.current_price = 125
+        p.sell_or_buy = :buy
+      end
+
+      policy.update_price(position, pair)
+      expect(policy.trailing_amount).to eq 124.5
+      position.current_price = 130
+      policy.update_price(position, pair)
+      expect(policy.trailing_amount).to eq 129.5
+      position.current_price = 129.8
+      policy.update_price(position, pair)
+      expect(policy.trailing_amount).to eq 129.5
+
+      policy = Jiji::Model::Trading::ClosingPolicy.create({
+        trailing_stop:   50,
+        trailing_amount: 0
+      })
+      position = Jiji::Model::Trading::Position.new do |p|
+        p.entry_price = 140
+        p.current_price = 135
+        p.sell_or_buy = :sell
+      end
+
+      policy.update_price(position, pair)
+      expect(policy.trailing_amount).to eq 135.5
+      position.current_price = 130.1
+      policy.update_price(position, pair)
+      expect(policy.trailing_amount).to eq 130.6
+      position.current_price = 130.4
+      policy.update_price(position, pair)
+      expect(policy.trailing_amount).to eq 130.6
+    end
+  end
+end
--- src/jiji/model/trading/closing_policy.rb
+++ src/jiji/model/trading/closing_policy.rb
@@ -37,6 +37,7 @@ module Jiji::Model::Trading
       end
     end
 
+    # for internal use.
     def extract_options_for_modify
       {
         stop_loss:     stop_loss,
@@ -45,11 +46,59 @@ module Jiji::Model::Trading
       }
     end
 
-    def close?(position)
-      false # TODO
+    # for internal use.
+    def should_close?(position)
+      should_take_profit?(position) \
+      || should_stop_loss?(position) \
+      || should_trailing_stop?(position)
+    end
+
+    # for internal use.
+    def update_price(position, pair)
+      return if trailing_stop == 0
+      price = BigDecimal.new(position.current_price, 10)
+      amount = (trailing_stop * pair.pip)
+      self.trailing_amount = calculate_trailing_amount(position, price, amount)
     end
 
     private
+
+    def calculate_trailing_amount(position, price, amount)
+      if position.sell_or_buy == :buy
+        new_price = (price - amount).to_f
+        trailing_amount == 0 ? new_price : [new_price, trailing_amount].max
+      else
+        new_price = (price + amount).to_f
+        trailing_amount == 0 ? new_price : [new_price, trailing_amount].min
+      end
+    end
+
+    def should_take_profit?(position)
+      return false if take_profit == 0
+      if position.sell_or_buy == :buy
+        return position.current_price >= take_profit
+      else
+        return position.current_price <= take_profit
+      end
+    end
+
+    def should_stop_loss?(position)
+      return false if stop_loss == 0
+      if position.sell_or_buy == :buy
+        return position.current_price <= stop_loss
+      else
+        return position.current_price >= stop_loss
+      end
+    end
+
+    def should_trailing_stop?(position)
+      return false if trailing_amount == 0
+      if position.sell_or_buy == :buy
+        return position.current_price <= trailing_amount
+      else
+        return position.current_price >= trailing_amount
+      end
+    end
 
     def values
       [

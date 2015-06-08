f5b47d25059c05c2097772bd86b20ae7f9edfb2a
Masaya Yamauchi <masaya.yamauchi@gmail.com>
2015-06-08 17:01:17 +0900

    建玉の決済処理のテストを追加。

--- spec/jiji/model/securities/internal/virtual/trading_spec.rb
+++ spec/jiji/model/securities/internal/virtual/trading_spec.rb
@@ -32,5 +32,194 @@ describe Jiji::Model::Securities::Internal::Virtual::Trading do
   it_behaves_like '建玉関連の操作'
 
   it 'レート更新時に、建玉が決済条件を満たすと決済される' do
+    saved_positions = position_repository.retrieve_positions(backtest_id)
+    expect(saved_positions.length).to be 0
+
+    rates1 = client.retrieve_current_tick
+
+    order1 = client.order(:EURJPY, :sell, 1, :market, {
+      stop_loss: 128.9
+    }).trade_opened
+    order2 = client.order(:USDJPY, :buy, 10, :market, {
+      take_profit: 119.97
+    }).trade_opened
+    order3 = client.order(:EURJPY, :sell, 2, :market, {
+      trailing_stop: 10
+    }).trade_opened
+
+    positions = client.retrieve_trades
+    expect(positions.length).to be 3
+
+    position = positions.find { |o| o.internal_id == order1.internal_id }
+    expect(position.pair_name).to eq :EURJPY
+    expect(position.units).to eq 1
+    expect(position.sell_or_buy).to eq :sell
+    expect(position.status).to eq :live
+    expect(position.entry_price).to eq rates1[:EURJPY].bid
+    expect(position.entered_at).not_to be rates1.timestamp
+    expect(position.current_price).to eq rates1[:EURJPY].ask
+    expect(position.updated_at).not_to be rates1.timestamp
+    expect(position.closing_policy.stop_loss).to eq(128.9)
+    expect(position.closing_policy.take_profit).to eq(0)
+    expect(position.closing_policy.trailing_stop).to eq(0)
+    expect(position.closing_policy.trailing_amount).to eq(0)
+
+    position = positions.find { |o| o.internal_id == order2.internal_id }
+    expect(position.pair_name).to eq :USDJPY
+    expect(position.units).to eq 10
+    expect(position.sell_or_buy).to eq :buy
+    expect(position.status).to eq :live
+    expect(position.entry_price).to eq rates1[:USDJPY].ask
+    expect(position.entered_at).not_to be rates1.timestamp
+    expect(position.current_price).to eq rates1[:USDJPY].bid
+    expect(position.updated_at).not_to be rates1.timestamp
+    expect(position.closing_policy.stop_loss).to eq(0)
+    expect(position.closing_policy.take_profit).to eq(119.97)
+    expect(position.closing_policy.trailing_stop).to eq(0)
+    expect(position.closing_policy.trailing_amount).to eq(0)
+
+    position = positions.find { |o| o.internal_id == order3.internal_id }
+    expect(position.pair_name).to eq :EURJPY
+    expect(position.units).to eq 2
+    expect(position.sell_or_buy).to eq :sell
+    expect(position.status).to eq :live
+    expect(position.entry_price).to eq rates1[:EURJPY].bid
+    expect(position.entered_at).not_to be rates1.timestamp
+    expect(position.current_price).to eq rates1[:EURJPY].ask
+    expect(position.updated_at).not_to be rates1.timestamp
+    expect(position.closing_policy.stop_loss).to eq(0)
+    expect(position.closing_policy.take_profit).to eq(0)
+    expect(position.closing_policy.trailing_stop).to eq(10)
+    expect(position.closing_policy.trailing_amount).to eq(0)
+
+    rates2 = client.retrieve_current_tick
+    positions = client.retrieve_trades
+    expect(positions.length).to be 3
+
+    position = positions.find { |o| o.internal_id == order1.internal_id }
+    expect(position.pair_name).to eq :EURJPY
+    expect(position.units).to eq 1
+    expect(position.sell_or_buy).to eq :sell
+    expect(position.status).to eq :live
+    expect(position.entry_price).to eq rates1[:EURJPY].bid
+    expect(position.entered_at).not_to be rates1.timestamp
+    expect(position.current_price).to eq rates2[:EURJPY].ask
+    expect(position.updated_at).not_to be rates2.timestamp
+    expect(position.closing_policy.stop_loss).to eq(128.9)
+    expect(position.closing_policy.take_profit).to eq(0)
+    expect(position.closing_policy.trailing_stop).to eq(0)
+    expect(position.closing_policy.trailing_amount).to eq(0)
+
+    position = positions.find { |o| o.internal_id == order2.internal_id }
+    expect(position.pair_name).to eq :USDJPY
+    expect(position.units).to eq 10
+    expect(position.sell_or_buy).to eq :buy
+    expect(position.status).to eq :live
+    expect(position.entry_price).to eq rates1[:USDJPY].ask
+    expect(position.entered_at).not_to be rates1.timestamp
+    expect(position.current_price).to eq rates2[:USDJPY].bid
+    expect(position.updated_at).not_to be rates2.timestamp
+    expect(position.closing_policy.stop_loss).to eq(0)
+    expect(position.closing_policy.take_profit).to eq(119.97)
+    expect(position.closing_policy.trailing_stop).to eq(0)
+    expect(position.closing_policy.trailing_amount).to eq(0)
+
+    position = positions.find { |o| o.internal_id == order3.internal_id }
+    expect(position.pair_name).to eq :EURJPY
+    expect(position.units).to eq 2
+    expect(position.sell_or_buy).to eq :sell
+    expect(position.status).to eq :live
+    expect(position.entry_price).to eq rates1[:EURJPY].bid
+    expect(position.entered_at).not_to be rates1.timestamp
+    expect(position.current_price).to eq rates2[:EURJPY].ask
+    expect(position.updated_at).not_to be rates2.timestamp
+    expect(position.closing_policy.stop_loss).to eq(0)
+    expect(position.closing_policy.take_profit).to eq(0)
+    expect(position.closing_policy.trailing_stop).to eq(10)
+    expect(position.closing_policy.trailing_amount).to eq(128.946)
+
+    3.times do |_i|
+      client.retrieve_current_tick
+      positions = client.retrieve_trades
+      expect(positions.length).to be 3
+    end
+
+    rates2 = client.retrieve_current_tick
+    positions = client.retrieve_trades
+    expect(positions.length).to be 2
+
+    position = positions.find { |o| o.internal_id == order1.internal_id }
+    expect(position.pair_name).to eq :EURJPY
+    expect(position.units).to eq 1
+    expect(position.sell_or_buy).to eq :sell
+    expect(position.status).to eq :live
+    expect(position.entry_price).to eq rates1[:EURJPY].bid
+    expect(position.entered_at).not_to be rates1.timestamp
+    expect(position.current_price).to eq rates2[:EURJPY].ask
+    expect(position.updated_at).not_to be rates2.timestamp
+    expect(position.closing_policy.stop_loss).to eq(128.9)
+    expect(position.closing_policy.take_profit).to eq(0)
+    expect(position.closing_policy.trailing_stop).to eq(0)
+    expect(position.closing_policy.trailing_amount).to eq(0)
+
+    position = positions.find { |o| o.internal_id == order2.internal_id }
+    expect(position).to be nil
+
+    position = positions.find { |o| o.internal_id == order3.internal_id }
+    expect(position.pair_name).to eq :EURJPY
+    expect(position.units).to eq 2
+    expect(position.sell_or_buy).to eq :sell
+    expect(position.status).to eq :live
+    expect(position.entry_price).to eq rates1[:EURJPY].bid
+    expect(position.entered_at).not_to be rates1.timestamp
+    expect(position.current_price).to eq rates2[:EURJPY].ask
+    expect(position.updated_at).not_to be rates2.timestamp
+    expect(position.closing_policy.stop_loss).to eq(0)
+    expect(position.closing_policy.take_profit).to eq(0)
+    expect(position.closing_policy.trailing_stop).to eq(10)
+    expect(position.closing_policy.trailing_amount).to eq(128.944)
+
+    3.times do |_i|
+      client.retrieve_current_tick
+      positions = client.retrieve_trades
+      expect(positions.length).to be 2
+    end
+
+    rates2 = client.retrieve_current_tick
+    positions = client.retrieve_trades
+    expect(positions.length).to be 1
+
+    position = positions.find { |o| o.internal_id == order1.internal_id }
+    expect(position).to be nil
+
+    position = positions.find { |o| o.internal_id == order2.internal_id }
+    expect(position).to be nil
+
+    position = positions.find { |o| o.internal_id == order3.internal_id }
+    expect(position.pair_name).to eq :EURJPY
+    expect(position.units).to eq 2
+    expect(position.sell_or_buy).to eq :sell
+    expect(position.status).to eq :live
+    expect(position.entry_price).to eq rates1[:EURJPY].bid
+    expect(position.entered_at).not_to be rates1.timestamp
+    expect(position.current_price).to eq rates2[:EURJPY].ask
+    expect(position.updated_at).not_to be rates2.timestamp
+    expect(position.closing_policy.stop_loss).to eq(0)
+    expect(position.closing_policy.take_profit).to eq(0)
+    expect(position.closing_policy.trailing_stop).to eq(10)
+    expect(position.closing_policy.trailing_amount).to eq(128.944)
+
+    92.times do |_i|
+      client.retrieve_current_tick
+      positions = client.retrieve_trades
+      expect(positions.length).to be 1
+    end
+
+    client.retrieve_current_tick
+    positions = client.retrieve_trades
+    expect(positions.length).to be 0
+
+    saved_positions = position_repository.retrieve_positions(backtest_id)
+    expect(saved_positions.length).to be 0
   end
 end

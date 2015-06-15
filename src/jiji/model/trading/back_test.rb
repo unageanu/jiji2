b29ea8ce6c07d1a455bc259422cae577f071d5cc
Masaya Yamauchi <masaya.yamauchi@gmail.com>
2015-06-15 10:17:33 +0900

    リファクタリング。

--- src/jiji/model/trading/back_test.rb
+++ src/jiji/model/trading/back_test.rb
@@ -7,60 +7,12 @@ require 'jiji/web/transport/transportable'
 require 'jiji/model/trading/internal/worker_mixin'
 
 module Jiji::Model::Trading
-  module BackTestFunctions
-    def load_broker_setting_from_hash(hash)
-      self.pair_names    = (hash['pair_names'] || []).map { |n| n.to_sym }
-      self.start_time    = hash['start_time']
-      self.end_time      = hash['end_time']
-      self.balance       = hash['balance'] || 0
-    end
-
-    private
-
-    def insert_broker_setting_to_hash(hash)
-      hash.merge({
-        pair_names: pair_names,
-        start_time: start_time,
-        end_time:   end_time,
-        balance:    balance
-      })
-    end
-
-    def create_components
-      graph_factory    = create_graph_factory
-      broker           = create_broker
-      @agents          = create_agents(
-        agent_setting, graph_factory, broker, id)
-      trading_context  = create_trading_context(broker, @agents, graph_factory)
-      @process         = create_process(trading_context)
-    end
-
-    def create_broker
-      pairs = (pair_names || []).map { |p| @pairs.get_by_name(p) }
-      Brokers::BackTestBroker.new(_id, start_time, end_time,
-        pairs, balance, @tick_repository)
-    end
-
-    def create_trading_context(broker, agents, graph_factory)
-      TradingContext.new(agents,
-        broker, graph_factory, time_source, @logger)
-    end
-
-    def create_process(trading_context)
-      Process.new(trading_context, backtest_thread_pool, true)
-    end
-  end
-
-  class BackTest
+  class BackTestProperties
 
     include Encase
-    include Jiji::Errors
-    include Jiji::Model::Trading
-    include Jiji::Model::Trading::Internal::WorkerMixin
     include Mongoid::Document
     include Jiji::Utils::ValueObject
     include Jiji::Web::Transport::Transportable
-    include BackTestFunctions
 
     needs :backtest_thread_pool
     needs :tick_repository
@@ -129,9 +81,24 @@ module Jiji::Model::Trading
         b.memo          = hash['memo']
         b.agent_setting = hash['agent_setting']
 
-        b.load_broker_setting_from_hash(hash)
+        load_broker_setting_from_hash(b, hash)
       end
     end
+
+    def self.load_broker_setting_from_hash(backtest, hash)
+      backtest.pair_names = (hash['pair_names'] || []).map { |n| n.to_sym }
+      backtest.start_time = hash['start_time']
+      backtest.end_time   = hash['end_time']
+      backtest.balance    = hash['balance'] || 0
+    end
+
+  end
+
+  class BackTest < BackTestProperties
+
+    include Jiji::Errors
+    include Jiji::Model::Trading
+    include Jiji::Model::Trading::Internal::WorkerMixin
 
     def setup
       self.created_at = time_source.now
@@ -146,6 +113,41 @@ module Jiji::Model::Trading
     def delete
       # TODO: delete positions, logs
       super
+    end
+
+    private
+
+    def insert_broker_setting_to_hash(hash)
+      hash.merge({
+        pair_names: pair_names,
+        start_time: start_time,
+        end_time:   end_time,
+        balance:    balance
+      })
+    end
+
+    def create_components
+      graph_factory    = create_graph_factory
+      broker           = create_broker
+      @agents          = create_agents(
+        agent_setting, graph_factory, broker, id)
+      trading_context  = create_trading_context(broker, @agents, graph_factory)
+      @process         = create_process(trading_context)
+    end
+
+    def create_broker
+      pairs = (pair_names || []).map { |p| @pairs.get_by_name(p) }
+      Brokers::BackTestBroker.new(_id, start_time, end_time,
+        pairs, balance, @tick_repository)
+    end
+
+    def create_trading_context(broker, agents, graph_factory)
+      TradingContext.new(agents,
+        broker, graph_factory, time_source, @logger)
+    end
+
+    def create_process(trading_context)
+      Process.new(trading_context, backtest_thread_pool, true)
     end
 
   end

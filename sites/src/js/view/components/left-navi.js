34c883bf41a5ddbb132a1a6ace17c470e0982d76
Masaya Yamauchi <masaya.yamauchi@gmail.com>
2015-06-17 15:39:14 +0900

    Material UIのバージョンアップで崩れていたスタイルを調整。
    * LeftNaviはMenuではなくListを使うように修正した。

--- sites/src/js/view/components/left-navi.js
+++ sites/src/js/view/components/left-navi.js
@@ -2,9 +2,10 @@ import React  from "react";
 import Router from "react-router";
 import MUI    from "material-ui";
 
-const RaisedButton = MUI.RaisedButton;
-const MenuItem     = MUI.MenuItem;
-const Menu         = MUI.Menu;
+const Types        = MUI.MenuItem.Types;
+const ListItem     = MUI.ListItem;
+const ListDivider  = MUI.ListDivider;
+const List         = MUI.List;
 
 export default class LeftNavi extends React.Component {
 
@@ -14,21 +15,49 @@ export default class LeftNavi extends React.Component {
   }
 
   render() {
+    const lists = this.createLists();
     return (
-      <Menu
-        ref="leftNav"
-        className="left-navi"
-        zDepth={0}
-        menuItems={this.navigator().menuItems()}
-        selectedIndex={this.getSelectedIndex()}
-        onItemTap={this.onLeftNavChange.bind(this)}
-      />
+      <div className="left-navi">
+        {lists}
+      </div>
     );
   }
 
-  getSelectedIndex() {
-    return this.navigator().getSelectedIndex(
-      (route) => this.router().isActive(route));
+  createLists() {
+    let lists  = [];
+    let buffer = [];
+    let label  = "";
+    this.navigator().menuItems().forEach((item)=> {
+      if (item.type === Types.SUBHEADER) {
+        lists.push(this.createList( label, buffer, lists.length));
+        lists.push(<ListDivider key={lists.length+"_divider"}/>);
+        buffer = [];
+        label  = item.text;
+      } else{
+        buffer.push( this.createListItem(item) );
+      }
+    });
+    lists.push(this.createList( label, buffer, lists.length));
+    return lists;
+  }
+
+  createList(label, items, index) {
+    return <List subheader={label} key={index}>{items}</List>;
+  }
+
+  createListItem(item, index) {
+    const selected = this.router().isActive(item.route);
+    const action   = (e) => this.onLeftNavChange(e, null, item);
+    const icon     = <div className={ "menu-icon " + item.iconClassName} />;
+    return (
+      <ListItem
+        key={item.route}
+        className={"mui-menu-item" + (selected ? " mui-is-selected" : "")}
+        leftIcon={icon}
+        onTouchTap={action}>
+        {item.text}
+      </ListItem>
+    );
   }
 
   onLeftNavChange(e, key, payload) {

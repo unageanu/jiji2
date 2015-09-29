cffc846e72eeb116230df5d8a15ea71216c927c1
unageanu <masaya.yamauchi@gmail.com>
2015-09-29 15:28:31 +0900

    ログ画面のデザインを精緻化

--- sites/src/js/view/components/logs/log-viewer.js
+++ sites/src/js/view/components/logs/log-viewer.js
@@ -1,6 +1,8 @@
 import React             from "react";
 import MUI               from "material-ui"
 import AbstractComponent from "../widgets/abstract-component";
+import LoadingImage      from "../widgets/loading-image"
+import Theme             from "../../theme"
 
 const FlatButton   = MUI.FlatButton;
 
@@ -26,16 +28,25 @@ export default class LogViewer extends AbstractComponent {
 
   render() {
     const pageSelectorElements = this.createPageSelectorElements();
-    const body = this.state.items && this.state.items.length > 0
-      ? this.state.items[0].body : "";
+    const body = this.createBodyContnet();
     return (
       <div className="log-viewer">
         <div className="page-selector">{pageSelectorElements}</div>
         <div className="body">
-          <pre>{body}</pre>
+          {body}
         </div>
       </div>
     );
+  }
+
+  createBodyContnet() {
+    if (this.state.items && this.state.items.length > 0) {
+      return <pre>{ this.state.items[0].body}</pre>;
+    } else {
+      return <div className="center-information loading">
+        <LoadingImage left={-20}/>
+      </div>;
+    }
   }
 
   createPageSelectorElements() {
@@ -47,13 +58,25 @@ export default class LogViewer extends AbstractComponent {
   createPageSelectorElement(selector, index) {
     if (selector.action) {
       const className = selector.selected ? "selected" : "";
+      const palette = Theme.getPalette();
       return (
         <FlatButton
-            className={className}
-            key={index}
-            label={""+selector.label}
-            onClick={selector.action}
-          />
+          className={"selector " + className}
+          key={index}
+          label={""+selector.label}
+          onClick={selector.action}
+          style={{
+            minWidth: "40px",
+            border: "1px solid " +
+              (selector.selected ? palette.accent1Color : palette.borderColor),
+            marginRight: "16px",
+            borderRadius: "0px"
+          }}
+          labelStyle={{
+            padding: "0px 8px",
+            color: selector.selected ? palette.accent1Color : palette.textColor
+          }}
+        />
       );
     } else {
       return <span
@@ -63,7 +86,6 @@ export default class LogViewer extends AbstractComponent {
       </span>;
     }
   }
-
 }
 LogViewer.propTypes = {
   model: React.PropTypes.object

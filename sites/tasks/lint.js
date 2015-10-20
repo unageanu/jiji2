59852bf97f4f187dc0e55cdd6b8a38dc9859a5b3
unageanu <masaya.yamauchi@gmail.com>
2015-10-20 19:54:15 +0900

    eslintのバージョンアップで、lintタスクが動作しなくなっていた問題を修正。
    * 拡張子が*.jsの場合、jsファイルとして解析が行われるようになり、パースエラーとなっていた。
    * 拡張子を変更して対応した。

--- sites/tasks/lint.js
+++ sites/tasks/lint.js
@@ -5,6 +5,6 @@ var eslint    = require('gulp-eslint');
 
 gulp.task('lint', function(cb) {
   return gulp.src(['src/js/**/*.js','spec/**/*.js'])
-      .pipe(eslint("./config/eslint.js"))
+      .pipe(eslint("./config/eslint.json"))
       .pipe(eslint.format());
 });

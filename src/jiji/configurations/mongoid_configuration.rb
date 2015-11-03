ceb6fa0c0cdba0602a108b7bc7c3c1a1375b6e45
unageanu <masaya.yamauchi@gmail.com>
2015-11-03 09:53:16 +0900

    Mongoidのログレベルを意図せずDEBUGにしていたので修正。

--- src/jiji/configurations/mongoid_configuration.rb
+++ src/jiji/configurations/mongoid_configuration.rb
@@ -7,8 +7,8 @@ require 'jiji/utils/requires'
 
 mongoid_setting_file = "#{Jiji::Utils::Requires.root}/config/mongoid.yml"
 
-Mongoid.logger.level = Logger::DEBUG
-Mongo::Logger.logger.level = Logger::DEBUG
+Mongoid.logger.level = Logger::WARN
+Mongo::Logger.logger.level = Logger::WARN
 
 if ENV['MONGOLAB_URI'] || ENV['MONGODB_URI']
   u = URI.parse(ENV['MONGOLAB_URI'] || ENV['MONGODB_URI'])

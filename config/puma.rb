3ad1b36bbc1fb6efae6fba46753f8aec3525f518
unageanu <masaya.yamauchi@gmail.com>
2015-11-04 09:48:50 +0900

    Dockerでの実行で必要なパラメータを追加。

--- config/puma.rb
+++ config/puma.rb
@@ -6,3 +6,8 @@ preload_app!
 rackup DefaultRackup
 port ENV['PORT']     || 5000
 environment ENV['RACK_ENV'] || 'production'
+
+daemonize ENV['PUMA_DAEMONIZE'] == 'true' || false
+
+app_path = ENV['PUMA_APPLICATION_PATH']
+directory app_path if !app_path.nil? && !app_path.empty?

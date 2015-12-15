cbc766de6fbc61ef63597b8941d183d8ade41cf7
unageanu <masaya.yamauchi@gmail.com>
2015-12-15 20:12:20 +0900

    MongoDBへの永続化をbulk_writeを使ってまとめて実行するユーティリティを作成。
    * BulkWriteOperationSupport#begin_transaction,#end_transaction の期間に #save された Document をスレッドローカルに記録し、#end_transaction でまとめてbulk writeする。
    * insertとupdateにのみ対応。delete には対応していない。

---
+++ spec/jiji/utils/bulk_write_operation_support_spec.rb
@@ -0,0 +1,243 @@
+# coding: utf-8
+
+require 'jiji/test/test_configuration'
+
+class TestModelA
+  include Mongoid::Document
+  include Jiji::Utils::BulkWriteOperationSupport
+
+  store_in collection: 'test_model_a'
+
+  field :name,      type: String
+  field :timestamp, type: Time
+end
+
+class TestModelB
+  include Mongoid::Document
+  include Jiji::Utils::BulkWriteOperationSupport
+
+  store_in collection: 'test_model_b'
+  belongs_to :test_model_a
+
+  field :name,      type: String
+  field :value,     type: Integer
+end
+
+describe Jiji::Utils::BulkWriteOperationSupport do
+
+  before(:example) do
+  end
+
+  after(:example) do
+    TestModelB.delete_all
+    TestModelA.delete_all
+  end
+
+  it 'トランザクション内で行われた更新を記録し、一括登録できる' do
+
+    expect(all_names_of_a).to eq []
+    expect(all_names_of_b).to eq []
+
+    Jiji::Utils::BulkWriteOperationSupport.begin_transaction
+
+    a1 = TestModelA.new
+    a1.name = "a1"
+    a2 = TestModelA.new
+    a2.name = "a2"
+    a1.save
+    a2.save
+
+    b1 = TestModelB.new
+    b1.name = "b1"
+    b1.test_model_a = a1
+    b2 = TestModelB.new
+    b2.name = "b2"
+    b2.test_model_a = a2
+    b1.save
+    b2.save
+
+    expect(all_names_of_a).to eq []
+    expect(all_names_of_b).to eq []
+
+    expect(Jiji::Utils::BulkWriteOperationSupport.transaction.size).to eq 4
+
+    Jiji::Utils::BulkWriteOperationSupport.end_transaction
+
+    expect(all_names_of_a).to eq ["a1","a2"]
+    expect(all_names_of_b).to eq ["b1","b2"]
+
+  end
+
+  it '永続化の順番が前後しても、親から順にソートして永続化できる' do
+
+    expect(all_names_of_a).to eq []
+    expect(all_names_of_b).to eq []
+
+    Jiji::Utils::BulkWriteOperationSupport.begin_transaction
+
+    a1 = TestModelA.new
+    a1.name = "a1"
+    a2 = TestModelA.new
+    a2.name = "a2"
+
+    b1 = TestModelB.new
+    b1.name = "b1"
+    b1.test_model_a = a1
+    b2 = TestModelB.new
+    b2.name = "b2"
+    b2.test_model_a = a2
+    b1.save
+    b2.save
+
+    a1.save
+    a2.save
+
+    expect(all_names_of_a).to eq []
+    expect(all_names_of_b).to eq []
+
+    expect(Jiji::Utils::BulkWriteOperationSupport.transaction.size).to eq 4
+
+    Jiji::Utils::BulkWriteOperationSupport.end_transaction
+
+    expect(all_names_of_a).to eq ["a1","a2"]
+    expect(all_names_of_b).to eq ["b1","b2"]
+
+  end
+
+  it '永続化済みドキュメントのupdateができる' do
+
+    a1 = TestModelA.new
+    a1.name = "a1"
+    a1.save
+    b1 = TestModelB.new
+    b1.name = "b1"
+    b1.test_model_a = a1
+    b1.save
+
+    expect(all_names_of_a).to eq ["a1"]
+    expect(all_names_of_b).to eq ["b1"]
+
+    Jiji::Utils::BulkWriteOperationSupport.begin_transaction
+
+    a2 = TestModelA.new
+    a2.name = "a2"
+    a2.save
+
+    b2 = TestModelB.new
+    b2.name = "b2"
+    b2.test_model_a = a2
+    b2.save
+
+    a1.name = "a1_2"
+    b1.name = "b1_2"
+    a1.save
+    b1.save
+
+    expect(all_names_of_a).to eq ["a1"]
+    expect(all_names_of_b).to eq ["b1"]
+
+    expect(Jiji::Utils::BulkWriteOperationSupport.transaction.size).to eq 4
+
+    Jiji::Utils::BulkWriteOperationSupport.end_transaction
+
+    expect(all_names_of_a).to eq ["a1_2","a2"]
+    expect(all_names_of_b).to eq ["b1_2","b2"]
+
+  end
+
+  it '複数回saveを呼び出しても、永続化は1度だけ行われる' do
+
+    a1 = TestModelA.new
+    a1.name = "a1"
+    a1.save
+    b1 = TestModelB.new
+    b1.name = "b1"
+    b1.test_model_a = a1
+    b1.save
+
+    expect(all_names_of_a).to eq ["a1"]
+    expect(all_names_of_b).to eq ["b1"]
+
+    Jiji::Utils::BulkWriteOperationSupport.begin_transaction
+
+    a2 = TestModelA.new
+    a2.name = "a2"
+    a2.save
+    a2.save
+    a2.save
+
+    b2 = TestModelB.new
+    b2.name = "b2"
+    b2.test_model_a = a2
+    b2.save
+    b2.save
+    b2.save
+
+    a1.name = "a1_2"
+    b1.name = "b1_2"
+    a1.save
+    b1.save
+    a1.save
+    b1.save
+
+    expect(all_names_of_a).to eq ["a1"]
+    expect(all_names_of_b).to eq ["b1"]
+
+    expect(Jiji::Utils::BulkWriteOperationSupport.transaction.size).to eq 4
+
+    Jiji::Utils::BulkWriteOperationSupport.end_transaction
+
+    expect(all_names_of_a).to eq ["a1_2","a2"]
+    expect(all_names_of_b).to eq ["b1_2","b2"]
+
+  end
+
+
+  it '更新がない場合、何もしない' do
+
+    a1 = TestModelA.new
+    a1.name = "a1"
+    a1.save
+    b1 = TestModelB.new
+    b1.name = "b1"
+    b1.test_model_a = a1
+    b1.save
+
+    expect(all_names_of_a).to eq ["a1"]
+    expect(all_names_of_b).to eq ["b1"]
+
+    Jiji::Utils::BulkWriteOperationSupport.begin_transaction
+
+    a1.save
+    b1.name = "b1_2"
+
+    expect(all_names_of_a).to eq ["a1"]
+    expect(all_names_of_b).to eq ["b1"]
+
+    expect(Jiji::Utils::BulkWriteOperationSupport.transaction.size).to eq 1
+
+    Jiji::Utils::BulkWriteOperationSupport.end_transaction
+
+    expect(all_names_of_a).to eq ["a1"]
+    expect(all_names_of_b).to eq ["b1"]
+
+  end
+
+  it '#in_transaction?' do
+    expect(Jiji::Utils::BulkWriteOperationSupport.in_transaction?).to be false
+    Jiji::Utils::BulkWriteOperationSupport.begin_transaction
+    expect(Jiji::Utils::BulkWriteOperationSupport.in_transaction?).to be true
+    Jiji::Utils::BulkWriteOperationSupport.end_transaction
+    expect(Jiji::Utils::BulkWriteOperationSupport.in_transaction?).to be false
+  end
+
+  def all_names_of(model_class)
+    model_class.all.map {|m| m.name }.sort
+  end
+  def all_names_of_a
+    all_names_of(TestModelA)
+  end
+  def all_names_of_b
+    all_names_of(TestModelB)
+  end
+end
---
+++ src/jiji/utils/bulk_write_operation_support.rb
@@ -0,0 +1,112 @@
+module Jiji::Utils
+  module BulkWriteOperationSupport
+
+    KEY = BulkWriteOperationSupport.name
+
+    def save
+      if BulkWriteOperationSupport.in_transaction?
+        BulkWriteOperationSupport.transaction << self
+      else
+        super
+      end
+    end
+
+    def self.in_transaction?
+      !transaction.nil?
+    end
+
+    def self.begin_transaction
+      Thread.current[KEY] = Transaction.new
+    end
+
+    def self.end_transaction
+      return unless in_transaction?
+      transaction.execute
+      Thread.current[KEY] = nil
+    end
+
+    def self.transaction
+      Thread.current[KEY]
+    end
+
+    def create_insert_operation
+      { :insert_one => as_document }
+    end
+
+    def create_update_operation
+      {
+        :update_one => {
+          :filter => { :_id => id },
+          :update => {'$set' => collect_changed_values }
+        }
+      }
+    end
+
+    private
+
+    def collect_changed_values
+      changes.each_with_object({}) do |change, r|
+        r[change[0].to_sym] = change[1][1]
+      end
+    end
+
+    class Transaction
+      def initialize
+        @targets = {}
+      end
+
+      def <<(model)
+        targets_of( model.class )[model.object_id] = model
+      end
+
+      def execute
+        until @targets.empty?
+          model_class = @targets.keys.first
+          execute_bulk_write_operations(model_class)
+        end
+      end
+
+      def size
+        @targets.values.reduce(0) {|r, v| r + v.length }
+      end
+
+      private
+
+      def targets_of( model_class )
+        @targets[model_class] ||= {}
+      end
+
+      def execute_bulk_write_operations(model_class)
+        return unless @targets.include?(model_class)
+        execute_parent_object_bulk_write_operations_if_exists(model_class)
+
+        client = model_class.mongo_client[model_class.collection_name]
+        operations = create_operations(@targets[model_class].values)
+        client.bulk_write(operations) unless operations.empty?
+
+        @targets.delete model_class
+      end
+
+      def execute_parent_object_bulk_write_operations_if_exists(model_class)
+        parents = model_class.reflect_on_all_associations(:belongs_to)
+        parents.each do |m|
+          klass = m.klass
+          execute_bulk_write_operations(klass)
+        end
+      end
+
+      def create_operations(targets)
+        targets.each_with_object([]) do |model, array|
+          if model.new_record?
+            model.new_record = false
+            array << model.create_insert_operation
+          else
+            array << model.create_update_operation if model.changed?
+          end
+        end
+      end
+
+    end
+
+  end
+end

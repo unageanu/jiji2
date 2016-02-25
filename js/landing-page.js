4738693397e5be2f3ae8b816ac19c63c15e6fb1f
unageanu <masaya.yamauchi@gmail.com>
2016-02-25 10:21:17 +0900

    Topページの訴求見直し。(途中)

--- js/landing-page.js
+++ js/landing-page.js
@@ -1,3 +1,17 @@
+$('#first-hook').on('inview', function(event, isInView) {
+  if (isInView) {
+    $('#site-header').removeClass("fixed animated fadeInDown");
+  }
+});
+
+$(window).scroll(function() {
+  if ($(document).scrollTop() > 50) {
+    $('#site-header').addClass('fixed animated fadeInDown');
+  } else {
+    $('#site-header').removeClass('fixed animated fadeInDown');
+  }
+});
+
 $('#closing').on('inview', function(event, isInView) {
   if (isInView) {
     $('#bottom_install_button').removeClass("hide").addClass("animated fadeInUp");

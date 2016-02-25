6249cfcbca3da12db97034b2936f92e633f3a651
unageanu <masaya.yamauchi@gmail.com>
2016-02-25 13:48:13 +0900

    bootstrap対応 (途中)

--- js/landing-page.js
+++ js/landing-page.js
@@ -1,8 +1,10 @@
-// $('#first-hook').on('inview', function(event, isInView) {
-//   if (isInView) {
-//     $('#site-header').removeClass("fixed animated fadeInDown");
-//   }
-// });
+$('#first-hook').on('inview', function(event, isInView) {
+  if (isInView) {
+    $('#site-header').removeClass("fixed animated fadeInDown");
+  } else {
+    $('#site-header').addClass('fixed animated fadeInDown');
+  }
+});
 //
 // $(window).scroll(function() {
 //   if ($(document).scrollTop() > 50) {

7ae4eaa23591a32e07a9586f9be0707f1409464a
unageanu <masaya.yamauchi@gmail.com>
2016-02-25 13:51:25 +0900

    Revert "bootstrap対応 (途中)"
    This reverts commit 6249cfcbca3da12db97034b2936f92e633f3a651.

--- js/landing-page.js
+++ js/landing-page.js
@@ -1,10 +1,8 @@
-$('#first-hook').on('inview', function(event, isInView) {
-  if (isInView) {
-    $('#site-header').removeClass("fixed animated fadeInDown");
-  } else {
-    $('#site-header').addClass('fixed animated fadeInDown');
-  }
-});
+// $('#first-hook').on('inview', function(event, isInView) {
+//   if (isInView) {
+//     $('#site-header').removeClass("fixed animated fadeInDown");
+//   }
+// });
 //
 // $(window).scroll(function() {
 //   if ($(document).scrollTop() > 50) {

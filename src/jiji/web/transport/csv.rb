2124e2838aab0e979184dea09a0919a939ef77fe
unageanu <masaya.yamauchi@gmail.com>
2016-03-15 09:46:35 +0900

    Refactor BatchDownloadSupport.
    * move formatting methods to web/transport/csv.rb.

---
+++ src/jiji/web/transport/csv.rb
@@ -0,0 +1,83 @@
+# coding: utf-8
+require 'time'
+require 'mongoid'
+
+class NilClass
+
+  def to_csv_value
+    ''
+  end
+
+end
+
+class Hash
+
+  def to_csv_value
+    to_json.to_csv_value
+  end
+
+end
+
+class Numeric
+
+  def to_csv_value
+    to_json.to_csv_value
+  end
+
+end
+
+class Array
+
+  def to_csv_value
+    to_json.to_csv_value
+  end
+
+end
+
+class String
+
+  def to_csv_value
+    include?(',') ? "\"#{gsub(/\"/, '""')}\"" : self
+  end
+
+end
+
+class Symbol
+
+  def to_csv_value
+    to_s.to_csv_value
+  end
+
+end
+
+class Time
+
+  def to_csv_value
+    iso8601.to_csv_value
+  end
+
+end
+
+class Struct
+
+  def to_csv_value
+    to_h.to_json.to_csv_value
+  end
+
+end
+
+class BigDecimal
+
+  def to_csv_value
+    to_f.to_json.to_csv_value
+  end
+
+end
+
+class BSON::ObjectId
+
+  def to_csv_value
+    to_s.to_csv_value
+  end
+
+end

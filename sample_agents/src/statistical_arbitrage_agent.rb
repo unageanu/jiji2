7f9ef668bbd933d662f56414d4920240b5072e40
unageanu <masaya.yamauchi@gmail.com>
2016-05-12 19:45:01 +0900

    Add QuandlResolver.

--- sample_agents/src/statistical_arbitrage_agent.rb
+++ sample_agents/src/statistical_arbitrage_agent.rb
@@ -1,3 +1,5 @@
+require 'quandl'
+require 'lru_redux'
 
 # === 統計的裁定取引エージェント
 class StatisticalArbitrageAgent
@@ -13,10 +15,11 @@ class StatisticalArbitrageAgent
   # UIから設定可能なプロパティの一覧
   def self.property_infos
     [
-      Property.new('pair1',  '通貨ペア1', "AUDJPY"),
-      Property.new('pair2',       '通貨ペア2', "NZDJPY"),
-      Property.new('trade_units', '取引数量', 10000),
-      Property.new('distance',    '取引を仕掛ける間隔(sdに対する倍率)', 1)
+      Property.new('pair1',          '通貨ペア1', "AUDJPY"),
+      Property.new('pair2',          '通貨ペア2', "NZDJPY"),
+      Property.new('trade_units',    '取引数量', 10000),
+      Property.new('distance',       '取引を仕掛ける間隔(sdに対する倍率)', 1),
+      Property.new('quandl_api_key', 'Quandl API KEY', ''),
     ]
   end
 
@@ -25,9 +28,15 @@ class StatisticalArbitrageAgent
       :line, :last, ['#779999'])
     rate_graph = graph_factory.create('rate',
       :rate, :last, ['#FF6633', '#FFAA22'])
+
     @trader = StatisticalArbitrage::CointegrationTrader.new(
       @pair1.to_sym, @pair2.to_sym, @trade_units.to_i, @distance.to_f,
       broker, spread_graph, rate_graph, logger)
+
+    if @quandl_api_key.nil? && @quandl_api_key.length > 0
+      @trader.cointegration_resolver =
+        StatisticalArbitrage::QuandlResolver.new(@quandl_api_key)
+    end
   end
 
   def next_tick(tick)
@@ -36,42 +45,31 @@ class StatisticalArbitrageAgent
 
 end
 
+module Enumerable
+
+    def sum
+      self.inject(0){|accum, i| accum + i }
+    end
+
+    def mean
+      self.sum/self.length.to_f
+    end
+
+    def sample_variance
+      m = self.mean
+      sum = self.inject(0){|accum, i| accum +(i-m)**2 }
+      sum/(self.length - 1).to_f
+    end
+
+    def standard_deviation
+      return Math.sqrt(self.sample_variance)
+    end
+
+end
+
 module StatisticalArbitrage
 
   module Utils
-
-    COINTEGRATIONS = {
-      '2014-01-01' => { slope:0.639779926, mean:39.798570231, sd:1.118107989 },
-      '2014-02-01' => { slope:0.472707933, mean:53.086706403, sd:1.410627152 },
-      '2014-03-01' => { slope:0.434243152, mean:56.061332315, sd:1.426766217 },
-      '2014-04-01' => { slope:0.388503806, mean:59.716515347, sd:1.417530649 },
-      '2014-05-01' => { slope:0.452009838, mean:54.565881819, sd:1.400824698 },
-      '2014-06-01' => { slope:0.468971690, mean:53.195204405, sd:1.345733500 },
-      '2014-07-01' => { slope:0.495815150, mean:51.029449076, sd:1.323011803 },
-      '2014-08-01' => { slope:0.507880741, mean:50.059333184, sd:1.289276140 },
-      '2014-09-01' => { slope:0.528603160, mean:48.425611954, sd:1.328614653 },
-      '2014-10-01' => { slope:0.565019583, mean:45.504254407, sd:1.441246028 },
-      '2014-11-01' => { slope:0.565114069, mean:45.575041006, sd:1.435544728 },
-      '2014-12-01' => { slope:0.658238976, mean:37.829951061, sd:1.591378385 },
-      '2015-01-01' => { slope:0.648219391, mean:38.666538200, sd:1.580403641 },
-      '2015-02-01' => { slope:0.621887956, mean:40.839250260, sd:1.587189027 },
-      '2015-03-01' => { slope:0.599479337, mean:42.612445521, sd:1.702255585 },
-      '2015-04-01' => { slope:0.557575116, mean:46.056167220, sd:1.819182217 },
-      '2015-05-01' => { slope:0.503979842, mean:50.493955291, sd:1.998065715 },
-      '2015-06-01' => { slope:0.500765214, mean:50.761523621, sd:1.961816777 },
-      '2015-07-01' => { slope:0.497107112, mean:51.135469439, sd:1.943820297 },
-      '2015-08-01' => { slope:0.503640515, mean:50.550317504, sd:1.923043502 },
-      '2015-09-01' => { slope:0.501153345, mean:50.760732076, sd:1.951388645 },
-      '2015-10-01' => { slope:0.628108957, mean:39.577199938, sd:2.051391140 },
-      '2015-11-01' => { slope:0.711480649, mean:32.131315959, sd:2.077261234 },
-      '2015-12-01' => { slope:0.759938317, mean:27.773159032, sd:2.049069654 },
-      '2016-01-01' => { slope:0.782435872, mean:25.723287669, sd:2.088435715 },
-      '2016-02-01' => { slope:0.837360002, mean:20.909344389, sd:2.163306779 },
-      '2016-03-01' => { slope:0.879965684, mean:17.162602491, sd:2.221233518 },
-      '2016-04-01' => { slope:0.875588818, mean:17.602128212, sd:2.218701436 },
-      '2016-05-01' => { slope:0.870884249, mean:17.985573263, sd:2.223409486 },
-      'latest'     => { slope:0.870884249, mean:17.985573263, sd:2.223409486 }
-    }
 
     def calculate_spread(pair1, pair2, tick, coint)
       price1 = tick[pair1].bid
@@ -137,6 +135,75 @@ module StatisticalArbitrage
       key = time.strftime("%Y-%m-01")
       COINTEGRATIONS[key] || COINTEGRATIONS["latest"]
     end
+  end
+
+  class QuandlResolver
+
+    include Utils
+
+    def initialize(api_key)
+      Quandl::ApiConfig.api_key = api_key if api_key
+      Quandl::ApiConfig.api_version = '2015-04-09'
+
+      @cache = LruRedux::ThreadSafeCache.new(10)
+    end
+
+    def resolve(time, pair1, pair2)
+      key = time.strftime("%Y-%m-%d")
+      @cache[key] || (@cache[key] = calculate_cointegration(time, pair1, pair2))
+    end
+
+    def calculate_cointegration(time, pair1, pair2)
+      rates = retrieve_rates(time, pair1, pair2)
+      linner_least_squares = linner_least_squares(rates)
+      spread = calculate_spread(rates, linner_least_squares)
+      return {
+        slope: linner_least_squares[0].to_f.round(9),
+        mean: spread.mean.to_f.round(9),
+        sd: spread.standard_deviation.to_f.round(9)
+      }
+    end
+
+    def calculate_spread(rates, linner_least_squares)
+      rates.map do |rate|
+        bd(rate[0]) - bd(rate[1]) * linner_least_squares[0]
+      end
+    end
+
+    def linner_least_squares(rates)
+      a = b = c = d = BigDecimal.new(0.0, 15)
+      rates.each do |r|
+        x = r[0]
+        y = r[1]
+        a += x*y
+        b += x
+        c += y
+        d += x**2
+      end
+      n = rates.size
+      return [(n*a-b*c)/(n*d-b**2),  (d*c-a*b)/(n*d-b**2)]
+    end
+
+    def retrieve_rates(time, pair1, pair2)
+      rates1 = retrieve_rates_from_quandl(time, pair1)
+      rates2 = retrieve_rates_from_quandl(time, pair2)
+      merged = {}
+      rates1.each {|rate| merged[rate["date"]] = [rate["rate"]] }
+      rates2.each do |rate|
+        merged[rate["date"]] << rate["rate"] if merged.include?(rate["date"])
+      end
+      return merged.values.reject {|d| d.length < 2 }
+    end
+
+    def retrieve_rates_from_quandl(time, pair)
+      Quandl::Dataset.get("CURRFX/#{pair.to_s}")
+        .data(params: {
+          rows: 1000,
+          start_date: (time - 2*365*24*60*60).strftime("%Y-%m-%d"),
+          end_date: time.strftime("%Y-%m-%d")
+        })
+    end
+
   end
 
   class CointegrationTrader

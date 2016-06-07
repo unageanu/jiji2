6349fc4a374b887f53c152e6be2e5f18437663a2
unageanu <masaya.yamauchi@gmail.com>
2016-06-07 16:43:56 +0900

    Extract the counter pair caluculation logic to CounterPairResolver.

---
+++ spec/jiji/model/trading/utils/counter_pair_resolver_spec.rb
@@ -0,0 +1,78 @@
+# coding: utf-8
+
+require 'jiji/test/test_configuration'
+require 'jiji/test/data_builder'
+
+describe Jiji::Model::Trading::Utils::CounterPairResolver do
+
+  include_context 'use data_builder'
+  let(:pairs) do
+    pairs = double('mock pairs')
+    allow(pairs).to receive(:all).and_return([
+      Jiji::Model::Trading::Pair.new(:EURUSD, 1, 0.01, 1, 1, 0.04),
+      Jiji::Model::Trading::Pair.new(:USDJPY, 1, 0.01, 1, 1, 0.04),
+      Jiji::Model::Trading::Pair.new(:EURJPY, 1, 0.01, 1, 1, 0.04),
+      Jiji::Model::Trading::Pair.new(:EURDKK, 1, 0.01, 1, 1, 0.04)
+    ])
+    pairs
+  end
+  let(:tick) do
+    Jiji::Model::Trading::Tick.new({
+      EURUSD: data_builder.new_tick_value(1),
+      USDJPY: data_builder.new_tick_value(2),
+      EURJPY: data_builder.new_tick_value(3),
+      EURDKK: data_builder.new_tick_value(4)
+    }, Time.utc(2015, 5, 1, 0, 0, 0))
+  end
+  let(:resolver) do
+    Jiji::Model::Trading::Utils::CounterPairResolver.new
+  end
+
+
+  describe '#resolve_rate' do
+    it 'returns the mid rate' do
+      expect(resolver.resolve_rate(tick, :EURUSD, 'JPY')).to eq 102.0015
+      expect(resolver.resolve_rate(tick, :CADEUR, 'JPY')).to eq 103.0015
+      expect(resolver.resolve_rate(tick, :CADEUR, 'USD')).to eq 101.0015
+
+      expect(resolver.resolve_rate(tick, :EURJPY, 'JPY')).to eq 1
+      expect(resolver.resolve_rate(tick, :CADEUR, 'EUR')).to eq 1
+      expect(resolver.resolve_rate(tick, :EURUSD, 'USD')).to eq 1
+
+      expect(resolver.resolve_rate(tick, :EURDKK, 'JPY')).to eq 0.990385
+    end
+  end
+
+  describe '#resolve_pair' do
+    it 'returns the counter pair.' do
+      expect(resolver.resolve_pair(:EURUSD, 'JPY')).to eq :USDJPY
+      expect(resolver.resolve_pair(:EURGBP, 'JPY')).to eq :GBPJPY
+      expect(resolver.resolve_pair(:USDEUR, 'JPY')).to eq :EURJPY
+      expect(resolver.resolve_pair(:USDJPY, 'JPY')).to eq :JPYJPY
+
+      expect(resolver.resolve_pair(:EURUSD, 'USD')).to eq :USDUSD
+      expect(resolver.resolve_pair(:EURGBP, 'USD')).to eq :GBPUSD
+      expect(resolver.resolve_pair(:USDEUR, 'USD')).to eq :EURUSD
+      expect(resolver.resolve_pair(:USDJPY, 'USD')).to eq :JPYUSD
+    end
+  end
+
+  describe '#resolve_required_pairs' do
+    it 'returns required pairs' do
+      expect(resolver.resolve_required_pairs(pairs, :EURUSD, 'JPY'))
+        .to eq [:USDJPY]
+      expect(resolver.resolve_required_pairs(pairs, :CADEUR, 'JPY'))
+        .to eq [:EURJPY]
+      expect(resolver.resolve_required_pairs(pairs, :CADEUR, 'USD'))
+        .to eq [:EURUSD]
+
+      expect(resolver.resolve_required_pairs(pairs, :EURJPY, 'JPY')).to eq []
+      expect(resolver.resolve_required_pairs(pairs, :CADEUR, 'EUR')).to eq []
+      expect(resolver.resolve_required_pairs(pairs, :EURUSD, 'USD')).to eq []
+
+      expect(resolver.resolve_required_pairs(pairs, :EURDKK, 'JPY'))
+        .to eq [:EURJPY, :EURDKK]
+    end
+  end
+
+end
---
+++ src/jiji/model/trading/utils/counter_pair_resolver.rb
@@ -0,0 +1,89 @@
+# coding: utf-8
+
+require 'set'
+
+module Jiji::Model::Trading::Utils
+
+  class Same
+    def match( available_pairs, steps )
+      steps[0] == steps[1]
+    end
+    def resolve_rate( tick, steps )
+      return 1
+    end
+    def resolve_required_pairs( available_pairs, steps )
+      return []
+    end
+  end
+
+  class IncludedInTick
+    def match( available_pairs, steps )
+      available_pairs.include?((steps[0]+steps[1]).to_sym)
+    end
+    def resolve_rate( tick, steps )
+      tick[(steps[0]+steps[1]).to_sym].mid
+    end
+    def resolve_required_pairs( available_pairs, steps )
+      return [(steps[0]+steps[1]).to_sym]
+    end
+  end
+
+  class NotIncludedInTick
+    def match( available_pairs, steps )
+      true
+    end
+    def resolve_rate( tick, steps )
+      pairs = resolve_required_pairs( tick, steps )
+      return (BigDecimal.new(tick[pairs[0]].mid, 10) \
+           / BigDecimal.new(tick[pairs[1]].mid, 10)).round(6)
+    end
+    def resolve_required_pairs( available_pairs, steps )
+      base = ["USD", "EUR"].find do |candidate|
+        a = (candidate+steps[1]).to_sym
+        b = (candidate+steps[0]).to_sym
+        available_pairs.include?(a) && available_pairs.include?(b)
+      end
+      return [(base+steps[1]).to_sym, (base+steps[0]).to_sym]
+    end
+  end
+
+  class CounterPairResolver
+
+    STRATEGIES = [
+      Same.new,
+      IncludedInTick.new,
+      NotIncludedInTick.new
+    ]
+
+    def resolve_rate(tick, pair_name, account_currency)
+      steps = resolve_counter_pairs(pair_name, account_currency)
+      find_strategy(tick, steps).resolve_rate(tick, steps)
+    end
+
+    def resolve_pair(pair_name, account_currency)
+      steps = resolve_counter_pairs(pair_name, account_currency)
+      steps.join.to_sym
+    end
+
+    def resolve_required_pairs(pairs, pair_name, account_currency)
+      available_pairs = pairs.all.each_with_object(Set.new) do |p, s|
+        s << p.name
+      end
+      steps = resolve_counter_pairs(pair_name, account_currency)
+      find_strategy(available_pairs, steps)
+        .resolve_required_pairs( available_pairs, steps )
+    end
+
+    private
+
+    def find_strategy(available_pairs, steps)
+      STRATEGIES.find {|s| s.match(available_pairs, steps) }
+    end
+
+    def resolve_counter_pairs(pair_name, account_currency)
+      [pair_name.to_s[3..6], account_currency]
+    end
+
+  end
+
+end

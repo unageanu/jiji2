21bdf53b481e9e09f3764212f1bac83bf92f3be0
unageanu <masaya.yamauchi@gmail.com>
2016-06-08 10:39:37 +0900

    Refactor PricingUtils.

--- src/jiji/model/trading/utils/pricing_utils.rb
+++ src/jiji/model/trading/utils/pricing_utils.rb
@@ -13,19 +13,17 @@ module Jiji::Model::Trading::Utils
         sell_or_buy == :buy ? :sell : :buy)
     end
 
-    def self.calculate_current_counter_rate(tick, pair_id, account_currency)
-      counter_pair_id = resolve_counter_pair_for(pair_id, account_currency)
-      return 1 if counter_pair_id.to_s[0..2] == counter_pair_id.to_s[3..6]
-      tick[counter_pair_id].mid
-    end
-
     def self.calculate_price(tick, pair_name, sell_or_buy)
       value = tick[pair_name]
       sell_or_buy == :buy ? value.ask : value.bid
     end
 
+    def self.calculate_current_counter_rate(tick, pair_name, account_currency)
+      CounterPairResolver.new.resolve_rate(tick, pair_name, account_currency)
+    end
+
     def self.resolve_counter_pair_for(pair_name, account_currency)
-      (pair_name.to_s[3..6] + account_currency).to_sym
+      CounterPairResolver.new.resolve_pair(pair_name, account_currency)
     end
 
   end

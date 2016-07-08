13905d5bfa985d983942199605f4e86bb5239003
unageanu <masaya.yamauchi@gmail.com>
2016-07-08 18:37:03 +0900

    Fix task name.

--- sites/tasks/watch.js
+++ sites/tasks/watch.js
@@ -4,6 +4,6 @@ var gulp    = require('gulp');
 
 gulp.task('watch', ['lint', 'build-debug', 'spec'], function(){
   gulp.watch('./src/less/**/*.less', ['build-less']);
-  gulp.watch('./src/js/**/*.js',     ['lint', 'build-js']);
+  gulp.watch('./src/js/**/*.js',     ['lint', 'build-debug-js']);
   gulp.watch('./spec/**/*.js',       ['lint', 'spec']);
 });

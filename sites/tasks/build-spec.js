ba2615e56c76154128bfd10fbac0c944d59d307e
unageanu <masaya.yamauchi@gmail.com>
2016-07-22 19:15:22 +0900

    Gulp Task: Fix configuration for spec.

--- sites/tasks/build-spec.js
+++ sites/tasks/build-spec.js
@@ -5,7 +5,7 @@ var webpack = require('webpack-stream');
 var configBuilder = require('../config/webpack.js');
 
 gulp.task('build-spec', ['prepare-build'], function () {
-  var config = configBuilder.src("development");
+  var config = configBuilder.spec("development");
   return gulp.src(config.entry)
       .pipe(webpack(config))
       .pipe(gulp.dest("./build/spec/js"));
